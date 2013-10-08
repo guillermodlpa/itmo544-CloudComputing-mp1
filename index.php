007d2dc4efa8aacae0ee446e9d0bc2cf3917fda8
gpuenteallott <g.puente.allott@gmail.com>
2013-10-08 17:28:13 -0500

    PHP files given by the teacher included

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+<h2>Picture Uploader</h2>
+<form action="process.php" method="post" enctype="multipart/form-data">
+  Email: <input type="text" name="email" > <br />
+  Cell Number: <input type="text" name="phone" placeholder="1-333-555-7777"> <br />
+ Choose Image: <input type="file" name="uploaded_file" id="uploaded_file"> <br />  
+  <input type="submit"  value="submit it!" >
+
+</body>
+
+</html>
