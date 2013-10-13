fd4dc94521bd6997b82d9b92817a83c47535a34e
gpuenteallott <g.puente.allott@gmail.com>
2013-10-13 18:53:10 -0500

    cleanup.php development started

--- cleanup.php
+++ cleanup.php
@@ -15,8 +15,97 @@
 # - Consume/Destroy Queue
 ################################################
 
-
+// Actions:
 // add code to consume the Queue to make sure the job is done
 // add code to send the SMS message of the finished S3 URL
 // Set object expire to remove the image in one day
 // set ACL to public
+
+
+// Include the SDK using the Composer autoloader
+require 'vendor/autoload.php';
+
+# Uncomment this for better debugging
+#header("Content-type: text/plain; charset=utf-8");
+
+use Aws\Common\Aws;
+use Aws\SimpleDb\SimpleDbClient;
+use Aws\S3\S3Client;
+use Aws\Sns\SnsClient;
+use Aws\Sqs\sqsclient;
+use Aws\Sns\Exception\InvalidParameterException;
+print "started";
+$aws = Aws::factory('./vendor/aws/aws-sdk-php/src/Aws/Common/Resources/custom-config.php');
+
+$client = $aws->get('S3'); 
+
+$sdbclient = $aws->get('SimpleDb'); 
+
+$snsclient = $aws->get('Sns'); 
+
+$sqsclient = $aws->get('Sqs');
+
+
+# Read the name file
+# Name is the resources identifier in AWS for this system
+$NAME = file_get_contents("name.txt");
+$NAME_SDB = str_replace("-", "", $NAME)."sdb";
+
+################################################
+# SQS
+# Delete the message to make sure it won't be processed two times
+################################################
+/*
+$result = $client->deleteMessage(array(
+    // QueueUrl is required
+    'QueueUrl' => "$NAME-sqs",
+    // ReceiptHandle is required
+    'ReceiptHandle' => 'string',
+));
+*/
+
+
+
+#############################################
+# Create SNS Simple Notification Service Topic for subscription
+##############################################
+/*
+$result = $snsclient->createTopic(array(
+    // Name is required
+    'Name' => $topic,
+));
+
+$topicArn = $result['TopicArn'];
+
+$result = $snsclient->setTopicAttributes(array(
+    // TopicArn is required
+    'TopicArn' => $topicArn,
+    // AttributeName is required
+    'AttributeName' => 'DisplayName',
+    'AttributeValue' => "$NAME",
+));
+
+try {
+$result = $snsclient->subscribe(array(
+
+    'TopicArn' => $topicArn,
+
+    'Protocol' => 'sms',
+    'Endpoint' => $phone,
+)); } catch(InvalidParameterException $i) {
+ echo 'Invalid parameter: '. $i->getMessage() . "\n";
+} 
+
+#####################################################
+# SNS publishing of message to topic - which will be sent via SMS
+#####################################################
+
+$result = $snsclient->publish(array(
+    'TopicArn' => $topicArn,
+    'TargetArn' => $topicArn,
+    // Message is required
+    'Message' => 'Your image has been uploaded',
+    'Subject' => $url,
+    'MessageStructure' => 'sms',
+));
+*/
